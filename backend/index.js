28d36e33a41905de415155449c5907a1e0de1238
Moraes Neto <moraesfelicioneto@hotmail.com>
2020-06-22 19:50:26 -0300

    iniciando projeto be-the-hero

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    return res.json({
+        nome: 'Moraes',
+        idade: 19
+    })
+});
+
+app.listen(3000);
+

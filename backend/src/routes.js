afe42300c60dfd7a325bc542ba4f7a008a9be55e
Moraes Neto <moraesfelicioneto@hotmail.com>
2020-06-24 17:24:52 -0300

    finalização das rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const routes = express.Router();
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController')
 
 routes.get('/ongs',OngController.index);
 routes.post('/ongs', OngController.create);
@@ -14,5 +15,6 @@ routes.delete('/incidents/:id', IncidentController.delete);
 
 
 routes.get('/profile', ProfileController.index);
+routes.post('/sessions', SessionController.create);
 
 module.exports = routes;
